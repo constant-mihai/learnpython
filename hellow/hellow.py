c2a515b56cc990f228db76ae49a116f81f8b50ee
Mihai Constantin <constant.mihai@googlemail.com>
2018-08-02 18:53:00 +0200

    Generates content automatically

--- hellow/hellow.py
+++ hellow/hellow.py
@@ -1,141 +1,211 @@
 #!/usr/bin/python3
 
+DESCR, CODE = 0, 1
+
+hellow = {
+        "Introduction" : 
+        {
+            "What is Python?" : 
+            [ 
+                ("Python is high-level interpreted *TODO(check the compiled AND"
+                "intepreted thing) language"),
+                None
+            ],
+            "Python implementations" : 
+            [
+                "Cpython, jython, PyPy, aso", None
+            ],
+
+        },
+        "Logging":
+        {
+            "Intro in logging":
+            [
+                ("Logging is cool. need to actually use txt files here\n"
+                "The logging first file is from:" 
+                "https://github.com/CoreyMSchafer"),
+                "logging_test.py"
+            ],
+            "More complicated logging":
+            [
+                ("Second logging file is still from:"
+                "https://github.com/CoreyMSchafer",),
+                "more_logging_test.py"
+
+            ]
+        },
+        "Data types and Variables":
+        {
+            "Some text about data types here":
+            [
+                "descr",
+                "data_types_and_variables.py"
+            ]
+        },
+        "Numbers in python":
+        {
+            "Numbers in python":
+            [
+                "descr",
+                "numbers_in_python.py"
+            ]
+        },
+        "Strings in python":
+        {
+            "Strings in python":
+            [
+                "descr",
+                "strings_in_python.py"
+            ],
+
+            "String methods in python":
+            [
+                "descr",
+                "string_methods_in_python.py"
+            ]
+        },
+        "Lists":
+        {
+            "Lists in python":
+            [
+                "descr",
+                "lists_in_python.py"
+            ],
+
+            "List methods in python":
+            [
+                "descr",
+                "list_methods_in_python.py"
+            ]
+        },
+        "Dictionaries":
+        {
+            "Dictionaries in python":
+            [
+                "descr",
+                "dictionaries_in_python.py"
+            ],
+
+            "Dictionarie methods in python":
+            [
+                "descr",
+                "dictionary_methods_in_python.py"
+            ]
+        },
+        "Functions":
+        {
+            "Functions in python":
+            [
+                "descr",
+                "functions_in_python.py"
+            ]
+        },
+        "Modules":
+        {
+            "Modules in python":
+            [
+                "descr",
+                "modules_in_python.py"
+            ]
+        },
+        "Classes":
+        {
+            "Clases in python":
+            [
+                "descr",
+                "objects_and_classes_in_python.py"
+            ]
+        },
+
+        
+}
+
+alphabet = "abcdefghijklmnopqrstuvwxyz" 
+
+#
+#
+#
+def get_i_from_alphabet(alpha):
+    if len(alpha) != 1:
+        raise Exception("len is not 1")
+
+    if alpha.isalpha() is False:
+        raise Exception("is not alpha")
+
+    alpha = alpha.lower()
+
+    i = 0
+    while alphabet[i] != "z": 
+        if alphabet[i] == alpha: return i
+        i+=1
+
+    raise Exception("This should not happen")
+
+#
+# Print description
+#
+def print_description(key, subkey):
+    if key == None or subkey == None:
+        raise Exception("Should not be null")
+
+    descr = hellow[key][subkey][DESCR]
+    if descr is None:
+        print("Description is missing!") 
+    else:
+        print(descr);
+
+#
+# Print file
+#
+def print_file(key, subkey):
+    if key == None or subkey == None:
+        raise Exception("Should not be null")
+
+    code = hellow[key][subkey][CODE]
+    if code is None:
+        print("Code file is missing!")
+    else:
+        f = open(code, "r");
+        print(f.read())
+
+#
+# Automatic
+#
+def automatic():
+    while True:
+        i = 0
+        associate = []
+        associate_subchapter = []
+        for key in hellow:
+            associate.append(key)
+            print("{}. {}".format(i, key))
+            i+=1
+
+        option = input("Chapter: ")
+
+        i = 0
+        opt_int = int(option)
+        print("{}. {}".format(opt_int, associate[opt_int]))
+        key = associate[opt_int]
+        for subch in hellow[key]:
+            associate_subchapter.append(subch)
+            print("\t{}) {}".format(alphabet[i], subch))
+            i+=1
+
+        subch_alpha = input("SubChapter: ")
+        subch_int   = get_i_from_alphabet(subch_alpha)
+
+        subkey = associate_subchapter[subch_int]
+        print("\t{}) {}".format(alphabet[subch_int], subkey))
+        print_description(key, subkey);
+        print_file(key, subkey)
+
 #
 # Main
 #
 def main():
-    print("Starting main.")
-    print("Choose lesson:")
-    print(" 0. Introduction")
-    print(" 1. Types and Variables")
-    print(" 2. Numbers in python")
-    print(" 3. Strings in python")
-    print(" 4. Lists")
-    print(" 5. Functions")
-    print(" 6. Modules")
-    print(" 7. Objects and classes")
-
-
-    option = input("Option: ")
-
-    if option == '0':
-        chapter_introduction()
-    elif option == "1":
-        chapter_variables()
-    elif option == "2":
-        chapter_numbers_in_python()
-    elif option == "3":
-        chapter_strings_in_python()
-    elif option == "4":
-        chapter_lists_in_python()
-    elif option == "5":
-        chapter_functions_in_python()
-    elif option == "6":
-        chapter_modules_in_python()
-    elif option == "7":
-        chapter_classes_in_python()
-
-#
-# chapter : Introduction
-#
-def chapter_introduction():
-    print("0. Introduction")
-
-    print(" a) What is Python?")
-    print(" b) Python implementations.")
-    subch = input("Choose subject:")
-
-    if subch == 'a':
-        print(" a) What is Python")
-        print(("Python is high-level interpreted *TODO(check the compiled AND"
-                "intepreted thing) language"))
-        print("Its also strongly typed.")
-    elif subch == 'b':
-        print(" b) Python implementations")
-        print("Cpython, jython, PyPy, aso")
-    else:
-        print(" Option unavailable")
-
-
-#
-# chapter : Variables
-#
-def chapter_variables():
-    print("1. Data types and Variables")
-    f = open("data_types_and_variables.py", "r");
-    contents = f.read()
-    print(contents)
-
-#
-# chapter : Numbers
-#
-def chapter_numbers_in_python():
-    print("2. Numbers in python")
-    f = open("numbers_in_python.py", "r")
-    contents = f.read()
-    print(contents)
-
-#
-# chapter : Strings 
-#
-def chapter_strings_in_python():
-    print("3. Strings in python")
-    print(" a) Strings:")
-    print(" b) String Methods:")
-
-    subch = input("Choose subject:")
-
-    if subch == 'a':
-        f = open("strings_in_python.py")
-        print(f.read())
-    elif subch == 'b':
-        f = open("string_methods_in_python.py")
-        print(f.read())
-    else:
-        print(" Option unavailable")
-
-#
-# chapter : Lists 
-#
-def chapter_lists_in_python():
-    print("4. Lists")
-    print(" a) Lists")
-    print(" b) List Methods:")
-    subch = input("Choose subject:")
-
-    if subch == 'a':
-        f = open("lists_in_python.py")
-        print(f.read())
-    elif subch == 'b':
-        f = open("list_methods_in_python.py")
-        print(f.read())
-    else:
-        print(" Option unavailable")
-
-#
-# chapter : Functions 
-#
-def chapter_functions_in_python():
-    print("5. Functions")
-    f = open("functions_in_python.py")
-    print(f.read())
-
-#
-# chapter : Modules 
-#
-def chapter_modules_in_python():
-    print("6. Modules")
-    f = open("modules_in_python.py")
-    print(f.read())
-
-#
-# chapter : Classes 
-#
-def chapter_classes_in_python():
-    print("7. Classes")
-    f = open("objects_and_classes_in_python.py")
-    print(f.read())
-
+    automatic()
+    
 #
 # Module check
 #
